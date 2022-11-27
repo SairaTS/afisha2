b0bb2dc2868d95ac8c91866de07d0b429d20c05c
SairaTS <saira.tashieva@gmail.com>
2022-11-28 00:53:41 +0600

    Completed firdt homework

---
+++ movie_app/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import *
+from .models import *
+
+# Create your views here.
+@api_view(['GET'])
+def director_view(request):
+    directors = Director.objects.all()
+    serializer = DirectortSerializer(directors, many=True)
+    return Response(data=serializer.data)
+
+@api_view(['GET'])
+def director_detail_view(request, id):
+    try:
+        director = Director.objects.get(id=id)
+    except Director.DoesNotExist:
+        return Response(data={'error': 'Director not found!'})
+    data = DirectortSerializer(director).data
+    return Response(data=data)
+
+@api_view(['GET'])
+def movie_view(request):
+    movies = Movie.objects.all()
+    serializer = MovieSerializer(movies, many=True)
+    return Response(data=serializer.data)
+
+@api_view(['GET'])
+def movie_detail_view(request, id):
+    try:
+        movie = Movie.objects.get(id=id)
+    except Movie.DoesNotExist:
+        return Response(data={'error:' 'Movie not found!'})
+    data = MovieSerializer(movie).data
+    return Response(data=data)
+
+@api_view(['GET'])
+def review_view(request):
+    reviews = Review.objects.all()
+    serializer = ReviewSerializer(reviews, many=True)
+    return Response(data=serializer.data)
+
+@api_view(['GET'])
+def review_detail_view(request, id):
+    try:
+        review = Review.objects.get(id=id)
+    except Review.DoesNotExist:
+        return Response(data={'error:' 'Review not found!'})
+    data = ReviewSerializer(review).data
+    return Response(data=data)
